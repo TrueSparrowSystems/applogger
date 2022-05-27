ad156ca10550ee9896039145382a2adc141de1e3
mohitcharkha <mohit@plgworks.com>
2022-05-27 15:37:14 +0530

    Used the common code from useLoggingFunctions

--- src/components/TouchableOpacity.tsx
+++ src/components/TouchableOpacity.tsx
@@ -1,28 +1,12 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {TouchableOpacity as RnTouchableOpacity} from 'react-native';
+import {ComponentTypes} from '../constants/ComponentTypes';
 import {useLoggingFunctions} from '../hooks/useLoggingFunctions';
 
 export function TouchableOpacity(props: any) {
-  const {onPress, onLongPress, onPressIn, onPressOut} =
-    useLoggingFunctions(props);
-
-  const filteredProps = useMemo(() => {
-    const propsCopy = {...props};
-    delete propsCopy.onPress;
-    delete propsCopy.onLongPress;
-    delete propsCopy.onPressIn;
-    delete propsCopy.onPressOut;
-    return propsCopy;
-  }, [props]);
+  const {filteredProps} = useLoggingFunctions(props, ComponentTypes.Button);
 
   return (
-    <RnTouchableOpacity
-      onPress={onPress}
-      onLongPress={onLongPress}
-      onPressIn={onPressIn}
-      onPressOut={onPressOut}
-      {...filteredProps}>
-      {props.children}
-    </RnTouchableOpacity>
+    <RnTouchableOpacity {...filteredProps}>{props.children}</RnTouchableOpacity>
   );
 }
--- src/components/TouchableWithoutFeedback.tsx
+++ src/components/TouchableWithoutFeedback.tsx
@@ -1,26 +1,13 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {TouchableWithoutFeedback as RnTouchableWithoutFeedback} from 'react-native';
+import {ComponentTypes} from '../constants/ComponentTypes';
 import {useLoggingFunctions} from '../hooks/useLoggingFunctions';
 
 export function TouchableWithoutFeedback(props: any) {
-  const {onPress, onLongPress, onPressIn, onPressOut} =
-    useLoggingFunctions(props);
-  const filteredProps = useMemo(() => {
-    const propsCopy = {...props};
-    delete propsCopy.onPress;
-    delete propsCopy.onLongPress;
-    delete propsCopy.onPressIn;
-    delete propsCopy.onPressOut;
-    return propsCopy;
-  }, [props]);
+  const {filteredProps} = useLoggingFunctions(props, ComponentTypes.Button);
 
   return (
-    <RnTouchableWithoutFeedback
-      onPress={onPress}
-      onLongPress={onLongPress}
-      onPressIn={onPressIn}
-      onPressOut={onPressOut}
-      {...filteredProps}>
+    <RnTouchableWithoutFeedback {...filteredProps}>
       {props.children}
     </RnTouchableWithoutFeedback>
   );
