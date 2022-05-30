d4091b76e77c6b7edb175f37a1f6ef51d8cf23b5
mohitcharkha <mohit@plgworks.com>
2022-05-30 13:09:35 +0530

    Updated props type in components

--- src/components/TouchableWithoutFeedback.tsx
+++ src/components/TouchableWithoutFeedback.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import {TouchableWithoutFeedback as RnTouchableWithoutFeedback} from 'react-native';
+import {
+  TouchableWithoutFeedback as RnTouchableWithoutFeedback,
+  TouchableWithoutFeedbackProps,
+} from 'react-native';
 import {ComponentTypes} from '../constants/ComponentTypes';
 import {useLoggingFunctions} from '../hooks/useLoggingFunctions';
 
-export function TouchableWithoutFeedback(props: any) {
+export function TouchableWithoutFeedback(props: TouchableWithoutFeedbackProps) {
   const {filteredProps} = useLoggingFunctions(props, ComponentTypes.Button);
 
   return (
