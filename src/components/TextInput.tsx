7d4c962a4a69032e809326299f964b1ddf89e75e
harshsiriah <harsh@plgworks.com>
2022-05-31 13:43:33 +0530

    PR review changes

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {TextInput as RnTextInput} from 'react-native';
+import {TextInput as RnTextInput, TextInputProps} from 'react-native';
 import {ComponentTypes} from '../constants/ComponentTypes';
 import {useLoggingFunctions} from '../hooks/useLoggingFunctions';
 
-export function TextInput(props: any) {
+export function TextInput(props: TextInputProps) {
   const {filteredProps} = useLoggingFunctions(props, ComponentTypes.TextInput);
 
   return <RnTextInput {...filteredProps}>{props.children}</RnTextInput>;
