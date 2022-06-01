bc908a400051bc096d68b39667175f9a627fc251
dhruvtailor7 <dhruv@plgworks.com>
2022-06-01 17:35:17 +0530

    Added navigation button on homescreen.

--- example/LogTrackerExample/src/components/RootNavigation/RootNavigation.tsx
+++ example/LogTrackerExample/src/components/RootNavigation/RootNavigation.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/HomeScreen';
+import SearchScreen from '../../screens/SearchScreen';
+import {RootStackParamList} from './types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigation() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="SearchScreen" component={SearchScreen} />
     </Stack.Navigator>
   );
 }
---
+++ example/LogTrackerExample/src/components/RootNavigation/types.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  SearchScreen: undefined;
+};
