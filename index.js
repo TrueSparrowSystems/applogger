10684fcd48639baad259edc7fbed8d73a5e4d8dc
mohitcharkha <mohit@plgworks.com>
2022-06-03 17:11:03 +0530

    Added few stying and other bug fixes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 export * from './src/components';
 export * from './src/constants';
 export * from './src/hooks';
-export {default as LogTracker} from './src/LogTracker';
+export {
+  default as LogTracker,
+  getLogTracker,
+  setConfig,
+} from './src/LogTracker';
 export {LogTrackerConfigInterface} from './src/LogTracker/LogTrackerConfigInterface';
 export {TrackInterface} from './src/LogTracker/TrackInterface';
